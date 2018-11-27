68160536a5e7b5da37ab3823cf1fc89d0a9c8c94
Charlles Abreu <abreu@eq.ufrj.br>
2018-11-27 17:16:17 -0500

    Initial commit

---
+++ Figures/figstyle.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+params = {
+	'font.size': 8,
+	'text.usetex': True,
+	'lines.linewidth': 0.5,
+	'legend.frameon': False,
+	'savefig.format': 'eps',
+	'savefig.dpi': 600,
+	'savefig.bbox': 'tight',
+}
+plt.rcParams.update(params)
+
